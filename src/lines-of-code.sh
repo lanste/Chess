fbf6e6e16c93dcf68ee79821ac6bc82f342a02fd
lanskteo <lanskteo@fit.cvut.cz>
2022-06-14 19:36:00 +0200

    more directory cleanup

--- src/lines-of-code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo $(date) >> progress.log
-RESULT=$(find "./src/" -type f -name "*.cpp" -o -name "*.h" | xargs wc -l | head -n -1 | wc -l)
-echo "$RESULT files of code" >> progress.log
-
-echo "$( find "./src/" -type f -name "*.cpp" -o -name "*.h" | xargs wc -l | tail -1 ) lines" >> progress.log
-echo "
-" >> progress.log
